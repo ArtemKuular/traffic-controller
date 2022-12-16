081c8fc7276b08ee89133554ffdea6e784186d19
artem <kuularartem02@gmail.com>
2022-12-16 18:09:19 +0300

    Создание файлов. Создание конструктора класса, добавление основных методов

---
+++ main.py
@@ -0,0 +1,11 @@
+import numpy as np
+import cv2
+import mediapipe as mp
+import time
+
+cap = cv2.VideoCapture(0)
+
+img0 = cv2.imread("images/0.png")
+img1 = cv2.imread("images/1.png")
+img2 = cv2.imread("images/2.png")
+img3 = cv2.imread("images/3.png")
