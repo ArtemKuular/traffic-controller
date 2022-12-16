dc380e702945670f537997367fcd50eddfa30eab
artem <kuularartem02@gmail.com>
2022-12-16 18:21:16 +0300

    Добавил изображения. Написал методы класса для распознавания позы человека. Начал писать основной цикл, добавил счетчик фпс

--- humanposemodule.py
+++ humanposemodule.py
@@ -1,12 +1,12 @@
 import cv2
 import mediapipe as mp
-import time
 import math
 
 
 class PoseDetector:
     def __init__(self, static_mode=False, model_complexity=1, upbbody=False, smooth=True, detection_confident=0.5,
                  tracking_confident=0.5):
+        self.lmlist = None
         self.results = None
         self.static_mode = static_mode
         self.complexity = model_complexity
@@ -22,14 +22,36 @@ class PoseDetector:
         self.mpdraw = mp.solutions.drawing_utils
 
     def find_pose(self, frame, draw_landmark=True):
-        pass
+        img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        self.results = self.pose.process(img)
+
+        if self.results.pose_landmarks:
+            if draw_landmark:
+                self.mpdraw.draw_landmarks(frame, self.results.pose_landmarks, self.mppose.POSE_CONNECTIONS)
 
     def find_location(self, frame, draw_landmark=True):
-        pass
+        self.lmlist = []
+        if self.results.pose_landmarks:
+            mypose = self.results.pose_landmarks
+
+            for idx, lm in enumerate(mypose.landmark):
+                h, w, c = frame.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+
+                self.lmlist.append([idx, cx, cy])
+                if draw_landmark:
+                    cv2.circle(frame, (cx, cy), 5, (255, 0, 255), cv2.FILLED)
+
+        return self.lmlist
 
     def find_angle(self, p1, p2, p3):
         pass
 
     def find_rotation(self):
-        pass
+        x1, y1 = self.lmlist[11][1:]
+        x2, y2 = self.lmlist[12][1:]
+        if x2 > x1:
+            return "back"
+        else:
+            return 'front'
 
--- main.py
+++ main.py
@@ -2,10 +2,49 @@ import numpy as np
 import cv2
 import mediapipe as mp
 import time
+import humanposemodule as hpm
 
 cap = cv2.VideoCapture(0)
+
+previous_time = 0
+current_time = 0
 
 img0 = cv2.imread("images/0.png")
 img1 = cv2.imread("images/1.png")
 img2 = cv2.imread("images/2.png")
 img3 = cv2.imread("images/3.png")
+
+posedetect = hpm.PoseDetector(detection_confident=0.8)
+
+while True:
+
+    check, frame = cap.read()
+
+    if check:
+
+        frame = cv2.resize(frame, (1280, 720))
+        frame = posedetect.find_pose(frame, draw_landmark=True)
+        lmlist = posedetect.find_location(frame, draw_landmark=True)
+
+        situation_id = 0
+
+        if len(lmlist) != 0:
+            pass
+
+        current_time = time.time()
+        fps = 1 / (current_time - previous_time)
+        previous_time = current_time
+
+        cv2.putText(frame, "frame rate: " + str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 5, (0, 255, 255), 5)
+
+        cv2.imshow('Traffic controller', frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    else:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
